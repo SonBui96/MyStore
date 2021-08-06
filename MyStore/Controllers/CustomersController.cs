f7af188a9b8362d05d4ca5cc5a0b4d90901337b8
ZEDFER <sontradercoin@gmail.com>
2021-08-06 12:34:45 +0700

    first commit

---
+++ MyStore/Controllers/CustomersController.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MyStore.Data;
+using MyStore.Models;
+
+namespace MyStore.Controllers
+{
+    public class CustomersController : Controller
+    {
+        private readonly MyStoreContext _context;
+
+        public CustomersController(MyStoreContext context)
+        {
+            _context = context;
+        }
+
+        [Authorize(Roles = SystemFunctions.ManageCustomer)]
+        // GET: Customers
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Customer.ToListAsync());
+        }
+
+        //[Authorize(Roles = SystemFunctions.ManageCustomer)]
+        // GET: Customers/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var customer = await _context.Customer
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+
+            return View(customer);
+        }
+
+        // GET: Customers/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Customers/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+
+                _context.Add(customer);
+
+                await _context.SaveChangesAsync();
+                var customerId = customer.Id;
+                return RedirectToAction(nameof(CongraCreated), new { id = customerId });
+            }
+            return View();
+        }
+
+        // GET: Customers/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var customer = await _context.Customer.FindAsync(id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+            return View(customer);
+        }
+
+        // POST: Customers/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, Customer customer)
+        {
+            if (id != customer.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(customer);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CustomerExists(customer.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Details), new { id = id });
+            }
+            return View(customer);
+        }
+
+        // GET: Customers/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var customer = await _context.Customer
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+
+            return View(customer);
+        }
+
+        // POST: Customers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var customer = await _context.Customer.FindAsync(id);
+            _context.Customer.Remove(customer);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CustomerExists(int id)
+        {
+            return _context.Customer.Any(e => e.Id == id);
+        }
+
+        public IActionResult CongraCreated(int Id)
+        {
+            ViewBag.ID = Id;
+            return View();
+        }
+
+        public IActionResult CustomerLogin(string returnUrl)
+        {
+            ViewBag.ReturnUrl = returnUrl;
+            return View();
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> CustomerLogin(CustomerLoginModel model, string returnUrl)
+        {
+            var validUser = await _context.Customer.FirstOrDefaultAsync(i => i.CustomerPhone == model.CustomerPhoneNumber);
+            if (validUser == null)
+            {
+                ModelState.AddModelError(string.Empty, "Số điện thoại chưa đăng ký hoặc nhập sai số điện thoại xin vui lòng thử lại hoặc đăng ký số điện thoại");
+            }
+            if (ModelState.IsValid)
+            {
+                var identity = new ClaimsIdentity(CookieAuthenticationDefaults.AuthenticationScheme);
+                identity.AddClaim(new Claim(ClaimTypes.MobilePhone, model.CustomerPhoneNumber));
+                identity.AddClaim(new Claim(CustomClaim.Fullname, validUser.CustomerName));
+                identity.AddClaim(new Claim(CustomClaim.Address, validUser.ShippingAddress));
+
+                var principal = new ClaimsPrincipal(identity);
+
+                await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, principal);
+
+                if (!string.IsNullOrWhiteSpace(returnUrl) && Url.IsLocalUrl(returnUrl))
+                {
+                    return Redirect(returnUrl);
+                }
+
+                return RedirectToAction("Index", "Home");
+            }
+
+            return View();
+        }
+    }
+}
---
+++ MyStore/Models/Customer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyStore.Models
+{
+    public class Customer : BaseDataModel
+    {
+        public Customer()
+        {
+
+        }
+        public int Id { get; set; }
+
+        public string CustomerPhone { get; set; }
+
+        public string CustomerName { get; set; }
+
+        public string ShippingAddress { get; set; }
+
+        public string RoleCode { get; set; }
+    }
+}
